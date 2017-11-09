ad5edf8b4b9720d70148a9043bd11470a72ab85b
Rico Chen <ricochen27@gmail.com>
2017-11-08 21:49:08 -0500

    linting

--- client/index.js
+++ client/index.js
@@ -1,16 +1,15 @@
-const fetch = require('node-fetch')
+const fetch = require('node-fetch');
 
-const doit = (id=0) => {
+const doit = (id = 0) => {
+    fetch(`http://localhost:8080/long?id=${id}`)
+        .then(res => res.text())
+        .then(val => {
+            console.log(`got result from remote: ${val}`);
+            return doit(val);
+        })
+        .catch(err => {
+            console.log('GOTERROR', err);
+        });
+};
 
-	fetch(`http://localhost:8080/long?id=${id}`)
-		.then(res => res.text())
-		.then(val => {
-			console.log(`got result from remote: ${val}`)
-			return doit(val)
-		})
-		.catch(err => {
-			console.log('GOTERROR', err)
-		})
-}
-
-doit()
+doit();
