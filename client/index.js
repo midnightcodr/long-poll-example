48f0d448f581a0408e5c489a5b4c81e0ce024a20
Rico Chen <ricochen27@gmail.com>
2017-11-09 08:36:14 -0500

    added retry

--- client/index.js
+++ client/index.js
@@ -1,15 +1,33 @@
 const fetch = require('node-fetch');
+const urlBase = 'http://localhost:8080/long?id='
+const bluebird = require('bluebird')
+const maxRetry = 10 
+const retryDelay = 3000	// ms
+
+// got retry idea from
+// https://github.com/jonbern/fetch-retry/blob/master/index.js
 
 const doit = (id = 0) => {
-    fetch(`http://localhost:8080/long?id=${id}`)
-        .then(res => res.text())
-        .then(val => {
-            console.log(`got result from remote: ${val}`);
-            return doit(val);
-        })
-        .catch(err => {
-            console.log('GOTERROR', err);
-        });
-};
+	let retries = maxRetry
+	var wrappedFetch = n => {
+		fetch(`${urlBase}${id}`)
+			.then(res => res.text())
+			.then(val => {
+				console.log(`got result from remote: ${val}`);
+				return doit(val);
+			})
+			.catch(err => {
+				if(n>0) {
+					console.log(`retry in ${retryDelay} ms`)
+					bluebird.delay(retryDelay).then(() => {
+						wrappedFetch(--n)
+					})
+				} else {
+					console.error(err)
+				}
+			})
+	}
+	wrappedFetch(retries)
+}
 
-doit();
+doit()
